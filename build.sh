8aa2770410c2c8a25f7fb9ec4b32e55a187d5577
Alexander Curtis <alexcu@tutanota.com>
2025-07-25 13:24:15 +0100

    fixed build script for install nightly if not installed

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# check if nightly installed
+if ! rustup show | grep -q 'nightly'; then
+  echo "Rust nightly not found. Installing..."
+  rustup install nightly
+fi
+
+# Run the build ussing nightly (a dependency of doing nostd bootloader fun!)
 cargo +nightly build -r
