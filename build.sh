d118c10485226746349de56c8e1b8cbaf6306994
Alexander Curtis <alexcu@tutanota.com>
2025-07-25 14:03:51 +0100

    fixed, added build script to insta boot

--- build.sh
+++ build.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
-set -e
 
-# check if nightly installed
+# Set required environment variables
+export CARGO_MANIFEST_DIR=$(pwd)
+
+# enforce  if nightly installed
 if ! rustup show | grep -q 'nightly'; then
   echo "Rust nightly not found. Installing..."
   rustup install nightly
 fi
 
-# Ensure bootimage is installed
+# enforce  bootimage is installed
 if ! cargo install --list | grep -q '^bootimage v'; then
   echo "'bootimage' not found. Installing..."
   cargo install bootimage
 fi
 
 
-# Run the build ussing nightly (a dependency of doing nostd bootloader fun!)
-cargo +nightly build -r
+# Run the build using nightly (a dependency of doing nostd bootloader fun!)
+echo "Building kernel"
+if ! cargo +nightly bootimage; then
+    echo "Error: Kernel build failed."
+    exit 1
+fi
 
 
-cargo +nightly bootimage
+#check if qemu is installed 
+
+if ! command -v qemu-system-x86_64 &> /dev/null; then
+    echo "qemu-system-x86_64 is not installed. Please install QEMU first."
+    exit 1
+fi
+
+
+QT_LOGGING_RULES="*.debug=false" qemu-system-x86_64 -drive format=raw,file=target/x86_64-nostd/debug/bootimage-theos.bin
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@
 #![no_std]
 #![no_main]
 #![cfg(not(test))] //disable tests (because otherwise we rust analyzer gets naggy!)
-use libc;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
-pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    unsafe { libc::printf(c"Hello world!\n".as_ptr()) };
-    0
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
 
 
@@ -15,4 +24,40 @@ pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+    unsafe {
+        for i in 0..n {
+            *dest.add(i) = *src.add(i);
+        }
+    }
+    dest
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
+    unsafe {
+        for i in 0..n {
+            *s.add(i) = c as u8;
+        }
+    }
+    s
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+    unsafe {
+        if src < dest as *const u8 {
+            for i in (0..n).rev() {
+                *dest.add(i) = *src.add(i);
+            }
+        } else {
+            for i in 0..n {
+                *dest.add(i) = *src.add(i);
+            }
+        }
+    }
+    dest
 }
