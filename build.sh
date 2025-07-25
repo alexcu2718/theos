2d65e2dc277f4e36cd085dfdb1d430d502f9e8e2
Alexander Curtis <alexcu@tutanota.com>
2025-07-25 17:27:48 +0100

    fixed build script for macos(not perfect yet)

--- build.sh
+++ build.sh
@@ -1,33 +1,36 @@
 #!/usr/bin/env bash
 
 
-# enforce  nightly installed
+# ensure rustup is available
+if ! command -v rustup &> /dev/null; then
+  echo "rustup not installed. Please install rustup first."
+  exit 1
+fi
+
+# enforce nightly installed (we need it )
 if ! rustup show | grep -q 'nightly'; then
   echo "Rust nightly not found. Installing..."
   rustup install nightly
 fi
 
-# enforce  bootimage is installed
+# Ensure bootimage is installed (for nightly)
 if ! cargo install --list | grep -q '^bootimage v'; then
   echo "'bootimage' not found. Installing..."
   cargo install bootimage
 fi
 
-
-# Run the build using nightly (a dependency of doing nostd bootloader fun!)
+# Build using nightly via rustup
 echo "Building kernel"
-if ! cargo +nightly bootimage; then
+if ! rustup run nightly cargo bootimage; then
     echo "Error: Kernel build failed."
     exit 1
 fi
 
-
-#check if qemu is installed 
-
+# Check QEMU is installed
 if ! command -v qemu-system-x86_64 &> /dev/null; then
     echo "qemu-system-x86_64 is not installed. Please install QEMU first."
     exit 1
 fi
 
-
+# Run the image
  qemu-system-x86_64 -drive format=raw,file=target/x86_64-nostd/debug/bootimage-theos.bin
